a1f4c1975a4a395ea2df69b3e59ebb3a4b61cf92
James Elsner <james.elsner@gmail.com>
2023-05-08 13:37:58 -0400

    target example

---
+++ _targets.R
@@ -0,0 +1,11 @@
+#_targets.R file
+# from https://books.ropensci.org/targets/walkthrough.html
+library(targets)
+source("R/functions.R")
+tar_option_set(packages = c("ggplot2"))
+list(
+  tar_target(file, "data.csv", format = "file"),
+  tar_target(data, get_data(file)),
+  tar_target(model, fit_model(data)),
+  tar_target(plot, plot_model(model, data))
+)
