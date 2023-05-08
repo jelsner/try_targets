8088105bb6a37b0cc078435ea1adcc1393a1551b
James Elsner <james.elsner@gmail.com>
2023-05-08 13:43:48 -0400

    data location change

--- _targets.R
+++ _targets.R
@@ -4,7 +4,7 @@ library(targets)
 source("R/functions.R")
 tar_option_set(packages = c("ggplot2"))
 list(
-  tar_target(file, "data.csv", format = "file"),
+  tar_target(file, here::here("data", "data.csv"), format = "file"),
   tar_target(data, get_data(file)),
   tar_target(model, fit_model(data)),
   tar_target(plot, plot_model(model, data))
